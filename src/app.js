37750fe18d84a9f816139bad1c9c420142995fab
HarshithaArava27 <165092618+HarshithaArava27@users.noreply.github.com>
2025-06-21 00:38:43 -0500

    routes both dynamic and non dynamic

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
 const express = require('express');
 
 const app = express();
+// non dynamic routes
 //handling the server
-app.get("/",(req,res)=>{
-    res.send("Hello from the server");
+app.get("/user",(req,res)=>{
+    console.log(req.query)
+    res.send({firstname:"Harshitha",lastname:"Saiteja"});
 });
+
+// for dynamic routes
+app.get("/user/:userId/:name/:pwd",(req,res)=>{
+    console.log(req.params)
+    res.send({firstname:"Harshitha",lastname:"Saiteja"});
+});
+
+app.post("/user",(req,res)=>{
+    res.send("post successfully");
+});
+
+app.delete("/user",(req,res)=>{
+    res.send("delete successfully");
+});
+
 //handling the server
-app.get("/hello",(req,res)=>{
+app.use("/hello",(req,res)=>{
     res.send(" hello from hello");
 });
 //handling the server
-app.get("/test",(req,res)=>{
+app.use("/test",(req,res)=>{
     res.send("Hello from the test");
+});
+//handling the server
+app.use("/",(req,res)=>{
+    res.send("Hello from the server");
 });
 
 //creating server in port 3000
